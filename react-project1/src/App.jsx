b4d6769e291b8b96204a4963df3ea3015c02d220
AmanBaghel09 <amanbaghel7489@gmail.com>
2025-12-06 22:38:29 +0530

    )1-folder done

---
+++ react-project1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const App = () => {
+  return (
+    <>
+
+    <div id='parent'>
+      <h1 id='child1'>APP</h1>
+      <h2 id='child2'>HELLO </h2>
+    </div>
+    <div className="grandparent"></div>
+     </>
+  )
+}
+
+export default App
---
+++ script.js
@@ -0,0 +1,5 @@
+// import username from './app.js'
+// import {age,skills} from './app.js'
+// console.log(username)
+// console.log(age)
+// console.log(skills)
