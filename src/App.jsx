122fc9175d9395e7d4c126f971b623fa7741a562
AmanBaghel09 <amanbaghel7489@gmail.com>
2025-12-07 15:21:21 +0530

    02-folder

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './components/card.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Navbar />
+      <Navbar />
+      <Card /> */}
+      <Card />
+    </div>
+  )
+}
+
+export default App
